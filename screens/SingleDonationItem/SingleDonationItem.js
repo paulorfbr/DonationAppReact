32f3b1c99569272d19a9a1fcc3bbaeb7ed53f9e2
PAULO FARIA <prfitsolutionsbe@gmail.com>
2025-09-12 16:22:33 +0200

    add navigation

--- screens/SingleDonationItem/SingleDonationItem.js
+++ screens/SingleDonationItem/SingleDonationItem.js
@@ -2,13 +2,47 @@ import React from 'react';
 
 import style from './style';
 import {useSelector} from 'react-redux';
+import {Image, SafeAreaView, ScrollView, View, Text} from 'react-native';
+import BackButton from '../../components/BackButton/BackButton';
+import Badge from '../../components/Badge/Badge';
+import Header from '../../components/Header/Header';
+import Button from '../../components/Button/Button';
 
-const SingleDonationItem = () => {
+import globalStyle from '../../assets/styles/globalStyle';
+
+const SingleDonationItem = ({navigation, route}) => {
+  const categoryInformation = route.params.categoryInformation;
   const donationItemInformation = useSelector(
     state => state.donations.selectedDonationInformation,
   );
   console.log(donationItemInformation);
-  return null;
+  return (
+    <SafeAreaView style={[globalStyle.backgroundWhite, globalStyle.flex]}>
+      <ScrollView showsVerticalScrollIndicator={false} style={style.container}>
+        <BackButton onPress={() => navigation.goBack()} />
+        <Image
+          source={{uri: donationItemInformation.image}}
+          style={style.image}
+        />
+        <View style={style.badge}>
+          <Badge title={categoryInformation.name} />
+        </View>
+        <Header type={1} title={donationItemInformation.name} />
+        <Text style={style.description}>
+          {donationItemInformation.description}
+          {donationItemInformation.description}
+          {donationItemInformation.description}
+          {donationItemInformation.description}
+          {donationItemInformation.description}
+          {donationItemInformation.description}
+          {donationItemInformation.description}
+        </Text>
+      </ScrollView>
+      <View style={style.button}>
+        <Button title={'Donate'} />
+      </View>
+    </SafeAreaView>
+  );
 };
 
 export default SingleDonationItem;
